c7150c9b07f8ccd4cdf309d4ff21c4b49af59420
vtech4034 <singh.vrp4034@gmail.com>
2023-11-06 08:49:03 -0800

    finall

--- php/fetchdepartment.php
+++ php/fetchdepartment.php
@@ -3,12 +3,8 @@ $conn = mysqli_connect("localhost","root","","company") or die("connection faile
 $sql="select *from department order by (did) asc";
 
 $result = mysqli_query($conn, $sql) or die("unreachable to fetch database");
+   $output=mysqli_fetch_all($result);
+    
+   echo json_encode($output);
 
-if(mysqli_num_rows($result)>0)
-{
-    while($row=mysqli_fetch_assoc($result))
-    {
-        echo "<h1>".$row['did']."=".$row['departmentname']."</h1></br>";
-    }
-}
 ?>
